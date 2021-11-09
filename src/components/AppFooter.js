598e8723c2e2e2d75ed21a6a8401fe9bf3f2858a
himanshu3w <vertigo.himanshu@gmail.com>
2021-11-09 07:11:16 +0530

    Changed App Footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Don Smart Finance
         </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
+        <span className="ms-1">&copy; 2021</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
+        <a href="https://dblockchainers.com/" target="_blank" rel="noopener noreferrer">
+          DBlockChainers
         </a>
       </div>
     </CFooter>
--- src/views/help/settings/changeemail/ChangeEmail.js
+++ src/views/help/settings/changeemail/ChangeEmail.js
@@ -1,5 +1,15 @@
 import React from 'react'
-import { CCard, CCardHeader, CCardBody } from '@coreui/react'
+import {
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CForm,
+  CFormLabel,
+  CFormText,
+  CFormInput,
+  CFormCheck,
+  CButton,
+} from '@coreui/react'
 import { DocsLink } from 'src/components'
 
 const ChangeEmail = () => {
@@ -8,6 +18,27 @@ const ChangeEmail = () => {
       <CCard className="mb-4">
         <div className="card-header text-center">Coming Soon [Settings -&gt; Change Email]</div>
       </CCard>
+      <CForm>
+        <div className="mb-3">
+          <CFormLabel htmlFor="exampleInputEmail1">Email address</CFormLabel>
+          <CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <CFormText id="emailHelp">Well never share your email with anyone else.</CFormText>
+        </div>
+        <div className="mb-3">
+          <CFormLabel htmlFor="exampleInputPassword1">Email Password</CFormLabel>
+          <CFormInput type="password" id="exampleInputPassword1" />
+        </div>
+        <CFormCheck
+          className="mb-3"
+          label="Check me out"
+          onChange={(e) => {
+            console.log(e.target)
+          }}
+        />
+        <CButton type="submit" color="primary">
+          Submit
+        </CButton>
+      </CForm>
     </>
   )
 }
